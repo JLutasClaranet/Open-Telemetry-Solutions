c2489e9b2b3597519f6c10a85d58386a68047b6d
João <joao.lutas@claranet.com>
2025-05-09 17:57:20 +0100

    + Global Providers Builder

--- TestOpenTelemetry/Docker/my-otel-grafana-setup/laravel/app/Providers/ObservabilityServiceProvider.php
+++ TestOpenTelemetry/Docker/my-otel-grafana-setup/laravel/app/Providers/ObservabilityServiceProvider.php
@@ -22,12 +22,22 @@ use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
 use OpenTelemetry\Contrib\Otlp\SpanExporter;
 use OpenTelemetry\SDK\Trace\TracerProvider;
 use OpenTelemetry\SDK\Trace\SpanProcessor\BatchSpanProcessor;
+use OpenTelemetry\SDK\Trace\Sampler\AlwaysOnSampler;
+use OpenTelemetry\SDK\Trace\Sampler\ParentBased;
+use OpenTelemetry\SDK\Sdk;
+use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
 //Logs
 use OpenTelemetry\SDK\Logs\LoggerProvider;
 use OpenTelemetry\Contrib\Otlp\LogsExporter;
 use OpenTelemetry\SDK\Logs\Processor\BatchLogRecordProcessor;
 use OpenTelemetry\SDK\Common\Instrumentation\InstrumentationScopeFactory;
 use OpenTelemetry\SDK\Common\Attribute\AttributesFactory;
+
+//METERS
+use OpenTelemetry\SDK\Metrics\MeterProvider;
+use OpenTelemetry\SDK\Metrics\MetricReader\ExportingReader;
+use OpenTelemetry\Contrib\Otlp\MetricExporter;
+use OpenTelemetry\SDK\Common\Export\Stream\StreamTransportFactory;
 
 
 
@@ -50,24 +60,66 @@ class ObservabilityServiceProvider extends ServiceProvider
         $traceExporter = new SpanExporter($traceTransport);
 
         // TracerProvider with correct argument order
-        $spanProcessor = new BatchSpanProcessor($traceExporter, $clock);
-        $tracerProvider = new TracerProvider($spanProcessor, null, $resource);
-        Globals::tracerProvider($tracerProvider);
+       // $spanProcessor = new BatchSpanProcessor($traceExporter, $clock);
+        //$tracerProvider = new TracerProvider($spanProcessor, null, $resource);
+        
+        $tracerProvider = TracerProvider::builder()
+        ->addSpanProcessor(
+            new BatchSpanProcessor($traceExporter, $clock)
+        )
+        ->setResource($resource)
+        ->setSampler(new ParentBased(new AlwaysOnSampler()))
+        ->build();
 
-        // Logger setup
-        $logsTransport = (new OtlpHttpTransportFactory())->create('http://otel-collector:4318/v1/logs', 'application/x-protobuf');
-        $logExporter = new LogsExporter($logsTransport);
 
-       
-        // Processor requires clock
-        $logProcessor = new BatchLogRecordProcessor($logExporter, $clock);
+      
+        /* Globals::tracerProvider($tracerProvider); */
 
-        // Create an AttributesFactory instance
-        $attributesFactory = new AttributesFactory();
-        $instrumentationScopeFactory = new InstrumentationScopeFactory($attributesFactory);
-        // Logger provider with resource
-        $loggerProvider = new LoggerProvider($logProcessor,$instrumentationScopeFactory, $resource);
-        Globals::loggerProvider($loggerProvider);
+        //LOGS
+         // Logger setup
+         $logsTransport = (new OtlpHttpTransportFactory())->create('http://otel-collector:4318/v1/logs', 'application/x-protobuf');
+         $logExporter = new LogsExporter($logsTransport);
+ 
+         // Processor requires clock
+         /* $logProcessor = new BatchLogRecordProcessor($logExporter, $clock);
+  */
+         // Create an AttributesFactory instance
+       /*   $attributesFactory = new AttributesFactory();
+         $instrumentationScopeFactory = new InstrumentationScopeFactory($attributesFactory); */
+         // Logger provider with resource
+        /*  $loggerProvider = new LoggerProvider($logProcessor,$instrumentationScopeFactory, $resource);
+         Globals::loggerProvider($loggerProvider); */
+
+         $loggerProvider = LoggerProvider::builder()
+            ->setResource($resource)
+            ->addLogRecordProcessor(
+                new BatchLogRecordProcessor($logExporter, $clock)
+            )
+            ->build();
+
+        //METRICS
+        $reader = new ExportingReader(
+            new MetricExporter(
+                (new StreamTransportFactory())->create('php://stdout', 'application/json')
+            )
+        );
+        
+
+        $meterProvider = MeterProvider::builder()
+            ->setResource($resource)
+            ->addReader($reader)
+            ->build();
+
+
+        //FULL BUILD
+        Sdk::builder()
+            ->setTracerProvider($tracerProvider)
+            ->setMeterProvider($meterProvider)
+            ->setLoggerProvider($loggerProvider)
+            ->setPropagator(TraceContextPropagator::getInstance())
+            ->setAutoShutdown(true)
+            ->buildAndRegisterGlobal();
+        
     }
 
     public function boot(): void
